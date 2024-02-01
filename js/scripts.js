140086b5dd204c930d2d850b38e8da4da1f6b849
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-02-01 14:12:40 -0300

    Finalizado o JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,17 @@ const btnShowMenu = document.querySelector(".show-menu");
 const btnApplyDiscount = document.querySelector(".apply-discount");
 const btnTotalOrder = document.querySelector(".total-order");
 const btnFilter = document.querySelector(".filter");
+
 const burgerContainer = document.querySelector(".burger-container");
 
-function showMenu() {
-  menuOptions.forEach((burger) => {
+const container = document.querySelector(".container");
+
+const priceBurger = document.querySelector(".price-burger");
+
+const showMenu = (products) => {
+  burgerContainer.innerHTML = "";
+
+  products.forEach((burger) => {
     const priceInReal = new Intl.NumberFormat("pt-br", {
       style: "currency",
       currency: "BRL",
@@ -18,15 +25,39 @@ function showMenu() {
       <p class="price-burger">${priceInReal}</p>
     </div> `;
   });
-}
+};
 
-function applyDiscount() {
-  menuOptions.map((discount) => {
-    discount.price = discount.price + discount.price * 0.1;
-    console.log(discount.price);
-    return discount;
-  });
-}
+const applyDiscount = () => {
+  const newPrices = menuOptions.map((product) => ({
+    ...product,
+    price: product.price * 0.9,
+  }));
 
-btnShowMenu.addEventListener("click", showMenu);
+  showMenu(newPrices);
+};
+
+const calculateTotal = () => {
+  const totalValue = menuOptions.reduce(
+    (acc, currentValue) => acc + currentValue.price,
+    0
+  );
+
+  const priceValue = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(totalValue);
+
+  burgerContainer.innerHTML = `<div class="total">
+        <h3>A soma de todos os itens do menu é ${priceValue}</h3>
+     </div>`;
+};
+
+const filterBurger = () => {
+  const filter = menuOptions.filter((product) => product.vegan);
+  showMenu(filter);
+};
+
+btnShowMenu.addEventListener("click", () => showMenu(menuOptions));
 btnApplyDiscount.addEventListener("click", applyDiscount);
+btnTotalOrder.addEventListener("click", calculateTotal);
+btnFilter.addEventListener("click", filterBurger);
